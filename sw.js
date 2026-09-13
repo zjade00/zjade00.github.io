b1ac12c4fe73a669d78ebaa188962b4d6ae8a581
zjade00 <zjade00@users.noreply.github.com>
2026-09-13 10:31:31 +0800

    Publish owner last update timestamp

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "customer-manager-v7-delete-r1";
+const CACHE = "customer-manager-v8-updated-r1";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (event) => {
